12b013c4faefe6ba584f25b11279820df441df3b
erikkaBaguio <erikka.baguio@gmail.com>
2016-05-09 22:18:18 +0800

    Added common steps for jsonify response in steps.py

---
+++ test/bdd/steps/steps.py
@@ -0,0 +1,24 @@
+from lettuce import step, world, before
+from nose.tools import assert_equals
+from webtest import *
+from app import app
+import json
+
+
+@before.all
+def before_all():
+    world.app = app.test_client()
+
+
+""" Common steps for jsonify response """
+
+
+@step(u'Then  it should have a \'([^\']*)\' response')
+def then_it_should_get_a_group1_response(step, expected_status_code):
+    assert_equals(world.response.status_code, int(expected_status_code))
+
+
+@step(u'And   it should have a field \'([^\']*)\' containing \'([^\']*)\'')
+def and_it_should_get_a_field_group1_containing_group2(step, field, expected_value):
+    world.response_json = json.loads(world.response.data)
+    assert_equals(str(world.response_json[field]), expected_value)
