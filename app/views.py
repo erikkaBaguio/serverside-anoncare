927bca024c2430eac306649033edf044528a9943
erikkaBaguio <erikka.baguio@gmail.com>
2016-05-10 09:12:01 +0800

    Modified functions.sql

--- app/views.py
+++ app/views.py
@@ -50,20 +50,6 @@ def load_token(token):
     return data[0] + ':' + data[1]
 
 
-def spcall(qry, param, commit=False):
-    try:
-        dbo = DBconn()
-        cursor = dbo.getcursor()
-        cursor.callproc(qry, param)
-        res = cursor.fetchall()
-        if commit:
-            dbo.dbcommit()
-        return res
-    except:
-        res = [("Error: " + str(sys.exc_info()[0]) + " " + str(sys.exc_info()[1]),)]
-    return res
-
-
 @auth.get_password
 def get_password(username):
     spcall = SPcalls()
--- db_scripts/functions.sql
+++ db_scripts/functions.sql
@@ -41,8 +41,7 @@ WHERE par_id = id;
 $$
 LANGUAGE 'sql';
 
-<<<<<<< HEAD
-=======
+
 --this will show the user information via username parameter
 create or replace function show_user_username(in par_username text, out text, out text, out text, out text, out text, out int) returns setof record as
   $$
@@ -67,7 +66,6 @@ create or replace function check_username_password(in par_username text, in par_
   $$
   language 'plpgsql';
 
->>>>>>> 1ae323c83bca9ac6dc1a2550034c7ceaf298f8c9
 --------------------------------------------------------------- USER -----------------------------------------------------------
 -- Check if user exists via username
 -- return 'OK' if user does not exist
@@ -338,194 +336,13 @@ $$
 
 language 'plpgsql';
 
---
--- --[GET] patient file
--- --select * from get_patientfileId(1);
--- CREATE OR REPLACE FUNCTION get_patientfileId(IN par_id INT, OUT TEXT, OUT TEXT, OUT TEXT, OUT INT, OUT TEXT,
---                                              OUT       TEXT, OUT FLOAT, OUT TEXT, OUT TEXT, OUT TEXT,
---                                              OUT       TEXT, OUT TEXT, OUT TEXT, OUT TEXT, OUT TEXT,
---                                              OUT       TEXT, OUT TEXT, OUT TEXT, OUT TEXT, OUT TEXT,
---                                              OUT       TEXT, OUT TEXT, OUT TEXT, OUT TEXT, OUT TEXT,
---                                              OUT       TEXT, OUT TEXT, OUT TEXT, OUT TEXT, OUT TEXT,
---                                              OUT       TEXT, OUT TEXT, OUT TEXT, OUT TEXT, OUT TEXT,
---                                              OUT       TEXT, OUT TEXT)
---   RETURNS SETOF RECORD AS
--- $$
--- SELECT
---   Patient.fname,
---   Patient.mname,
---   Patient.lname,
---   Patient.age,
---   Patient.sex,
---   Personal_info.height,
---   Personal_info.weight,
---   Personal_info.date_of_birth,
---   Personal_info.civil_status,
---   Personal_info.name_of_guardian,
---   Personal_info.home_address,
---   Pulmonary.cough,
---   Pulmonary.dyspnea,
---   Pulmonary.hemoptysis,
---   Pulmonary.tb_exposure,
---   Gut.frequency,
---   Gut.flank_plan,
---   Gut.discharge,
---   Gut.dysuria,
---   Gut.nocturia,
---   Gut.dec_urine_amount,
---   Illness.asthma,
---   Illness.ptb,
---   Illness.heart_problem,
---   Illness.hepatitis_a_b,
---   Illness.chicken_pox,
---   Illness.mumps,
---   Illness.typhoid_fever,
---   Cardiac.chest_pain,
---   Cardiac.palpitations,
---   Cardiac.pedal_edema,
---   Cardiac.orthopnea,
---   Cardiac.nocturnal_dyspnea,
---   Neurologic.headache,
---   Neurologic.seizure,
---   Neurologic.dizziness,
---   Neurologic.loss_of_consciousness
--- FROM Patient, Personal_info, Pulmonary, Gut, Illness, Cardiac, Neurologic
--- WHERE Patient.id = par_id AND Personal_info.id = Patient.personal_info_id AND Pulmonary.id = Patient.pulmonary_id AND
---       Gut.id = Patient.gut_id AND Illness.id = Patient.illness_id AND Cardiac.id = Patient.cardiac_id AND
---       Neurologic.id = Patient.neurologic_id;
--- $$
--- LANGUAGE 'sql';
---
--- --[GET] Retrieve the type of patient.
--- --select getpatienttypeID(1);
--- CREATE OR REPLACE FUNCTION getpatienttypeID(IN par_id INT, OUT TEXT)
---   RETURNS TEXT AS
--- $$
--- SELECT type
--- FROM Patient_type
--- WHERE id = par_id;
--- $$
--- LANGUAGE 'sql';
---
--- -----------------------------------------------------END of Patient File --------------------------------------------------
---
--- -------------------------------------------------------- Assessment -------------------------------------------------------
--- -- [POST] Create new assessment
--- --select new_assessment(1,20130000, 37.1, 80, 19, '90/70', 48, 'complaint', 'history', 'medication1', 'diagnosis1','recommendation1', 1);
--- --select new_assessment(2,20130001, 36.4, 70, 19, '100/80', 45, 'complaint', 'history', 'medication1', 'diagnosis1','recommendation1', 1);
--- CREATE OR REPLACE FUNCTION new_assessment(IN par_id                       INT,
---                                           IN par_schoolID                 INT,
---                                           IN par_temperature              FLOAT,
---                                           IN par_pulse_rate               FLOAT,
---                                           IN par_respiration_rate         INT,
---                                           IN par_blood_pressure           TEXT,
---                                           IN par_weight                   FLOAT,
---                                           IN par_chiefcomplaint           TEXT,
---                                           IN par_historyofpresentillness  TEXT,
---                                           IN par_medicationstaken         TEXT,
---                                           IN par_diagnosis                TEXT,
---                                           IN par_recommendation           TEXT,
---                                           IN par_attendingphysician       INT)
---   RETURNS TEXT AS
--- $$
--- DECLARE
---   loc_id1       INT;
---   loc_id2       INT;
---   loc_res       TEXT;
--- BEGIN
---   SELECT INTO loc_id1 id
---   FROM Assessment
---   WHERE id = par_id;
---
---   SELECT INTO loc_id2 id
---   FROM Vital_signs
---   WHERE id = par_id;
---
---   IF loc_id1 ISNULL AND loc_id2 ISNULL
---   THEN
---     IF par_chiefcomplaint = '' OR
---        par_chiefcomplaint ISNULL OR
---        par_medicationstaken = '' OR
---        par_medicationstaken ISNULL OR
---        par_diagnosis = '' OR
---        par_diagnosis ISNULL
---     THEN
---       loc_res = 'PLEASE FILL THE REQUIRE FIELDS';
---
---     ELSE
---         INSERT INTO Vital_signs(id, temperature, pulse_rate, respiration_rate, blood_pressure, weight)
---         VALUES (par_id,par_temperature, par_pulse_rate, par_respiration_rate, par_blood_pressure, par_weight);
---
---         INSERT INTO Assessment (id,school_id, vital_signsID, chiefcomplaint, historyofpresentillness,
---                                 medicationstaken, diagnosis, recommendation, attendingphysician)
---         VALUES (par_id,par_schoolID, par_id, par_chiefcomplaint, par_historyofpresentillness, par_medicationstaken,
---                 par_diagnosis, par_recommendation, par_attendingphysician);
---
---         loc_res = 'OK';
---
---     END IF;
---   ELSE
---       loc_res = 'ID EXISTS';
---   END IF;
---   RETURN loc_res;
---
--- END;
--- $$
---   LANGUAGE 'plpgsql';
-
-
--- [GET] Retrieve assessment of a specific patient
---select getassessmentID(20130000,1);
--- CREATE OR REPLACE FUNCTION show_assessment_id(IN par_schoolID INT,
---                                            IN par_id INT,
---                                            OUT BIGINT,
---                                            OUT TIMESTAMP,
---                                            OUT INT,
---                                            OUT INT,
---                                            OUT INT,
---                                            OUT TEXT,
---                                            OUT TEXT,
---                                            OUT TEXT,
---                                            OUT TEXT,
---                                            OUT TEXT,
---                                            OUT INT,
---                                            OUT BOOLEAN,
---                                            OUT FLOAT,
---                                            OUT FLOAT,
---                                            OUT INT,
---                                            OUT TEXT,
---                                            OUT FLOAT,
---                                            OUT TEXT)
---   RETURNS SETOF RECORD AS
--- $$
---
--- select Assessment.*,
---          Vital_signs.temperature,
---          Vital_signs.pulse_rate,
---          Vital_signs.respiration_rate,
---          Vital_signs.blood_pressure,
---          Vital_signs.weight,
---          Userinfo.fname,
---          Userinfo.lname
---   FROM Assessment
---   INNER JOIN Vital_signs ON (
---     Assessment.vital_signsID = Vital_signs.id
---     )
---   INNER JOIN Userinfo ON (
---     Assessment.attendingphysician = Userinfo.id
---     )
---   WHERE Assessment.id = par_id
---   AND Assessment.school_id = par_schoolID
---
--- $$
--- LANGUAGE 'sql';
-
-
+-----------------------------------------------------END of Patient File --------------------------------------------------
 -- [GET] Retrieve all assessment of a specific patient
 --select getallassessmentID(20130000);
 CREATE OR REPLACE FUNCTION getallassessmentID(IN par_schoolID INT,
-                                           OUT INT,
+                                           OUT BIGINT,
                                            OUT TIMESTAMP,
+                                           OUT INT,
                                            OUT INT,
                                            OUT INT,
                                            OUT TEXT,
@@ -564,35 +381,6 @@ $$
 
 $$
   LANGUAGE 'sql';
-
--- --[PUT] Update assessment of patient
--- --select update_assessment(1,20130000, 'medication1f', 'diagnosis11f','recommendation11', 1);
--- CREATE OR REPLACE FUNCTION update_assessment(IN par_id                 INT,
---                                              IN par_schoolID           TEXT,
---                                              IN par_medicationstaken   TEXT,
---                                              IN par_diagnosis          TEXT,
---                                              IN par_recommendation     TEXT,
---                                              IN par_attendingphysician INT)
---   RETURNS TEXT AS
--- $$
--- DECLARE
---   loc_res TEXT;
--- BEGIN
---
---   UPDATE Assessment
---   SET
---     diagnosis          = par_diagnosis,
---     recommendation     = par_recommendation,
---     attendingphysician = par_attendingphysician
---   WHERE id = par_id
---   AND school_id = par_schoolID;
---
---   loc_res = 'Updated';
---   RETURN loc_res;
---
--- END;
--- $$
---   LANGUAGE 'plpgsql';
 
 
 ------------------------------------------------------------ ASSESSMENTS -----------------------------------------------------------
